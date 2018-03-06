b62683917633d00364524139abdb4faca0d5f56d
Jade Paoletta <jpaoletta@Jades-MacBook-Pro.local>
2018-03-06 13:16:19 -0800

    Added set method to Restaurant class

--- restuarant_classes.py
+++ restuarant_classes.py
@@ -1,9 +1,10 @@
 
 class Restaurant():
 
-    def __init__(self, name, cuisine):
+    def __init__(self, name, cuisine, served=0):
         self.name = name
         self.cuisine = cuisine
+        self.served = served
 
     def describe_restaurant(self):
         """Returns a short description of the restaruant"""
@@ -13,6 +14,17 @@ class Restaurant():
         """Indicates that the restaurant is now open"""
 
         print "{} is now Open!".format(self.name.title())
+
+    def number_served(self):
+        """prints the number of customers served"""
+
+        print "{} has served {} customers!".format(self.name.title(), self.served)
+
+    def set_number_served(self, num_served):
+        """increments the number served value"""
+
+        self.served += num_served
+
 
 
 chapeau = Restaurant("chapeau", "french") 
